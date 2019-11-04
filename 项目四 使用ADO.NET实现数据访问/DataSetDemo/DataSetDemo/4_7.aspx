1a794f5c98af6fb452ed67179361f7465f26a1cb
imewp <mewp@vip.qq.com>
2019-11-04 11:14:05 +0800

    4_7 4_8

---
+++ 项目四 使用ADO.NET实现数据访问/DataSetDemo/DataSetDemo/4_7.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="4_7.aspx.cs" Inherits="DataSetDemo._4_7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        数据绑定<hr/>
+        成员变量值：<%# uName %><br/>
+        成员方法返回值：<%# NowTime() %><br/>
+        计算表达式：<%# "ASP"+".NET" %><br/>
+        控件属性：<%# Label1.Text %><br/>
+        虚拟应用程序根路径：<%# Request.ApplicationPath %><br/>
+        配置文件数据：<asp:Label ID="Label1" runat="server" Text="<% $AppSettings:test %>"></asp:Label>
+        <br/><br/><br/><br/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 项目四 使用ADO.NET实现数据访问/DataSetDemo/DataSetDemo/4_8.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using System.Data;
+
+namespace DataSetDemo
+{
+    public partial class _4_8 : System.Web.UI.Page
+    {
+        /// <summary>
+        /// 集合数据绑定，将查询的商品类型名称分别
+        /// 绑定到DropDownList和CheckBoxList控件
+        /// </summary>
+        protected void SetBind()
+        {
+            string str = ConfigurationManager.ConnectionStrings["smdb"].ConnectionString;
+            DataSet ds = new DataSet();
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                string cmdTest = "select * from goodsType";
+                SqlDataAdapter sda = new SqlDataAdapter(cmdTest, conn);
+                sda.Fill(ds, "gtype");
+            }
+            ddlGoodType.DataSource = ds.Tables["gtype"];
+            ddlGoodType.DataTextField = "tName";
+            ddlGoodType.DataValueField = "tID";
+            ddlGoodType.DataBind();
+            chkGoodType.DataSource = ds.Tables["gtype"];
+            chkGoodType.DataTextField = "tName";
+            chkGoodType.DataValueField = "tID";
+            chkGoodType.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetBind();
+        }
+    }
+}
