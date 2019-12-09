4b33d207ac5ee9ec7b02aaa7d9d35a23925d0780
wp <1572819519@qq.com>
2019-12-09 11:30:00 +0800

    修改CacheDemo

--- 项目七 高速缓存、跟踪检测和站点部署/CacheDemo/CacheDemo/7_2.aspx.cs
+++ 项目七 高速缓存、跟踪检测和站点部署/CacheDemo/CacheDemo/7_2.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Caching;       //导入缓冲所需的命名空间
 
 namespace CacheDemo
 {
     public partial class _7_2 : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        public static void setCache(string cacheKey,object value,SqlCacheDependency scDep)
+        {
+            HttpRuntime.Cache.Insert(cacheKey, value, scDep, Cache.NoAbsoluteExpiration, Cache.NoAbsoluteExpiration);
         }
     }
 }
